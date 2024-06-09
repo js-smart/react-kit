4689676287c3264049fc773a7b4733eed0a67490
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-06-09 10:27:02 -0400

    feat(core): updates NPM deps

--- apps/react-kit-demo/src/routes/root.tsx
+++ apps/react-kit-demo/src/routes/root.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import Home from '../app/Home';
 
 export default function Root() {
-  return (
-    <>
-      <h1 style={{ textAlign: 'center' }}>React Kit Demo</h1>
-      <hr />
+	return (
+		<>
+			<h1 style={{ textAlign: 'center' }}>React Kit Demo</h1>
+			<hr />
 
-      <p>
-        This is a demo of the <code>@react-kit</code> package.
-      </p>
-      <div id="detail">
-        <Outlet />
-      </div>
-    </>
-  );
+			<Home />
+		</>
+	);
 }
