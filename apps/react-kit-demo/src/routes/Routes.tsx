f73ee767234af4ecb3328ff61ec86c684c01c6c4
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-07-17 16:50:18 -0400

    feat(core): updates routes

---
+++ apps/react-kit-demo/src/routes/Routes.tsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../app/Home';
+import ButtonsDemo from '../app/buttons/ButtonsDemo';
+import SnackBarDemo from '../app/snack-bar/SnackBarDemo';
+import DialogDemo from '../app/dialog/DialogDemo';
+import CenterCircularProgressDemo from '../app/progress-bar/CenterCircularProgressDemo';
+import AllBooks from '../app/all-books/AllBooks';
+import App from '../app/app';
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+			{
+				path: 'home',
+				element: <Home />,
+			},
+			{
+				path: '/buttons',
+				element: <ButtonsDemo />,
+			},
+			{
+				path: '/snack-bar',
+				element: <SnackBarDemo />,
+			},
+			{
+				path: '/dialog',
+				element: <DialogDemo />,
+			},
+			{
+				path: '/circular-progress',
+				element: <CenterCircularProgressDemo />,
+			},
+			{
+				path: '/books',
+				element: <AllBooks />,
+			},
+		],
+	},
+]);
