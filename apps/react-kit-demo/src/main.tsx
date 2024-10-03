7bed955d012a8e613393befe3e3fa85a7a8832ff
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-10-03 12:20:46 -0400

    feat(core): adds ccs variables support

--- apps/react-kit-demo/src/main.tsx
+++ apps/react-kit-demo/src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/Routes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	</StrictMode>
 );
---
+++ apps/react-kit-demo/src/theme.ts
@@ -0,0 +1,59 @@
+import { createTheme } from '@mui/material/styles';
+import { getCssVariable } from './utils/CssUtils';
+
+declare module '@mui/material/styles' {
+	interface BreakpointOverrides {
+		xs: true;
+		sm: true;
+		md: true;
+		lg: true;
+		xl: true;
+		xxl: true;
+		xxxl: true;
+	}
+}
+
+// A custom theme for this app
+const theme = createTheme({
+	cssVariables: { cssVarPrefix: '' },
+	breakpoints: {
+		values: {
+			xs: 0,
+			sm: 600,
+			md: 900,
+			lg: 1200,
+			xl: 1536,
+			xxl: 1920,
+			xxxl: 2200,
+		},
+	},
+	palette: {
+		primary: {
+			main: getCssVariable('--primary-color'),
+		},
+		secondary: {
+			main: getCssVariable('--secondary-color'),
+		},
+		success: {
+			main: getCssVariable('--success-color'),
+		},
+		error: {
+			main: getCssVariable('--error-color'),
+		},
+		background: {
+			default: getCssVariable('--background-color'),
+		},
+	},
+	components: {
+		// @ts-ignore
+		MuiDataGrid: {
+			styleOverrides: {
+				root: {
+					'--DataGrid-containerBackground': '#FFFFFF',
+				},
+			},
+		},
+	},
+});
+
+export default theme;
