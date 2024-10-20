abd620d0a387e016e8c483938db4665f752a9b72
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-10-20 00:26:19 -0400

    feat(core): upgrades MUI to v6
    BREAKING CHANGE: Upgrades MUI version to v6

---
+++ react-kit/src/tests/utils/NumberUtils.test.ts
@@ -0,0 +1,19 @@
+import { parseNumber } from '../../lib/utils/NumberUtils';
+
+describe('parseNumber', () => {
+	it('should return a number when a valid string is provided', () => {
+		expect(parseNumber('123')).toBe(123);
+	});
+
+	it('should return NaN when an invalid string is provided', () => {
+		expect(parseNumber('abc')).toBeNaN();
+	});
+
+	it('should return undefined when null is provided', () => {
+		expect(parseNumber(null)).toBeUndefined();
+	});
+
+	it('should return undefined when undefined is provided', () => {
+		expect(parseNumber(undefined)).toBeUndefined();
+	});
+});
