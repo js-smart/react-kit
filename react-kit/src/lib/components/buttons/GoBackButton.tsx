e9a327de7c6b792b3865bc88fb858ca4919ad6a4
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-10-20 00:47:30 -0400

    fix(GoBackButton): fixes type issue

--- react-kit/src/lib/components/buttons/GoBackButton.tsx
+++ react-kit/src/lib/components/buttons/GoBackButton.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { IconButton, Tooltip } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useNavigate } from 'react-router-dom';
 
 interface GoBackButtonProps {
-	onClick: () => void;
 	name?: string;
 	children?: React.ReactNode;
 }
 
 export function GoBackButton(props: GoBackButtonProps) {
+	const navigate = useNavigate();
 	return (
 		<Tooltip title="Go Back to Previous Page">
-			<IconButton name={props.name} color="primary" onClick={() => props.onClick()}>
+			<IconButton name={props.name} color="primary" onClick={() => navigate(-1)}>
 				<ArrowBackIosIcon />
-				{props.children ?? props.name}
+				{props.children}
 			</IconButton>
 		</Tooltip>
 	);
