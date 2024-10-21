a115565fe07f4f7a8565f84f32d0c704d8413c53
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-10-21 18:09:06 -0400

    chore(tests): updates cert

--- apps/react-kit-demo/vite.config.ts
+++ apps/react-kit-demo/vite.config.ts
@@ -2,48 +2,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/react-kit-demo',
+	root: __dirname,
+	cacheDir: '../../node_modules/.vite/apps/react-kit-demo',
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
+	server: {
+		port: 3007,
+		host: 'localhost',
+	},
 
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
+	preview: {
+		port: 4300,
+		host: 'localhost',
+	},
 
-  plugins: [react(), nxViteTsPaths()],
+	plugins: [react(), mkcert(), nxViteTsPaths()],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
 
-  build: {
-    outDir: '../../dist/apps/react-kit-demo',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
+	build: {
+		outDir: '../../dist/apps/react-kit-demo',
+		reportCompressedSize: true,
+		commonjsOptions: {
+			transformMixedEsModules: true,
+		},
+	},
 
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest/apps/react-kit-demo',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	test: {
+		globals: true,
+		cache: {
+			dir: '../../node_modules/.vitest/apps/react-kit-demo',
+		},
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/react-kit-demo',
-      provider: 'v8',
-    },
-  },
+		reporters: ['default'],
+		coverage: {
+			reportsDirectory: '../../coverage/apps/react-kit-demo',
+			provider: 'v8',
+		},
+	},
 });
