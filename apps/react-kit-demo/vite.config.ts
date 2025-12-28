19f6a8ffeb7af3f9254053cc50617155d3248fe1
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-12-28 15:27:08 -0500

    fix: downgrade esbuild to 0.26.0 and update pnpm scripts configuration  (#42)

--- apps/react-kit-demo/vite.config.ts
+++ apps/react-kit-demo/vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	root: __dirname,
--- react-kit/src/lib/components/buttons/HistoryButton.tsx
+++ react-kit/src/lib/components/buttons/HistoryButton.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button, SxProps, Theme } from "@mui/material";
-import HistoryIcon from "@mui/icons-material/History";
+import HistoryIcon from '@mui/icons-material/History';
+import { Button, SxProps, Theme } from '@mui/material';
+import React from 'react';
 
 /**
  * Reusable History Button component properties
@@ -9,16 +9,16 @@ import HistoryIcon from "@mui/icons-material/History";
  * @since 1.2.15
  */
 interface HistoryButtonProps {
-  children?: React.ReactNode;
-  className?: string;
-  name?: string;
-  dataCy?: string;
-  sx?: SxProps<Theme>;
-  type?: "button" | "submit" | "reset";
-  onClick: () => void;
-  startIcon?: React.ReactNode;
-  variant?: "text" | "outlined" | "contained";
-  color?: "inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning";
+	children?: React.ReactNode;
+	className?: string;
+	name?: string;
+	dataCy?: string;
+	sx?: SxProps<Theme>;
+	type?: 'button' | 'submit' | 'reset';
+	onClick: () => void;
+	startIcon?: React.ReactNode;
+	variant?: 'text' | 'outlined' | 'contained';
+	color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
 }
 
 /**
@@ -28,19 +28,18 @@ interface HistoryButtonProps {
  * @since 1.2.15
  */
 export function HistoryButton(props: HistoryButtonProps): React.JSX.Element {
-  return (
-    <Button
-      name={props.name}
-      data-cy={props.dataCy ?? "history-button"}
-      className={props.className}
-      sx={props.sx ?? { p: 1, m: 1 }}
-      startIcon={props.startIcon ?? <HistoryIcon />}
-      variant={props.variant ?? "contained"}
-      color={props.color ?? "primary"}
-      type={props.type ?? "button"}
-      onClick={() => props.onClick()}
-    >
-      {props.children ?? props.name}
-    </Button>
-  );
+	return (
+		<Button
+			name={props.name}
+			data-cy={props.dataCy ?? 'history-button'}
+			className={props.className}
+			sx={props.sx ?? { p: 1, m: 1 }}
+			startIcon={props.startIcon ?? <HistoryIcon />}
+			variant={props.variant ?? 'contained'}
+			color={props.color ?? 'primary'}
+			type={props.type ?? 'button'}
+			onClick={() => props.onClick()}>
+			{props.children ?? props.name}
+		</Button>
+	);
 }
