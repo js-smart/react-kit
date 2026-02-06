c26d9c8d357415bed9920e76d12b86eb64a33794
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2026-02-06 13:05:56 -0500

    refactor: adjust import statements and formatting in `ButtonsDemo` and `DeleteButton` components for consistency

--- apps/react-kit-demo/src/app/buttons/ButtonsDemo.tsx
+++ apps/react-kit-demo/src/app/buttons/ButtonsDemo.tsx
@@ -1,3 +1,4 @@
+import { Divider } from '@mui/material';
 import {
   CancelButton,
   DeleteButton,
@@ -7,65 +8,51 @@ import {
   LoadingSuccessButton,
   ManageButton,
   SuccessButton,
-} from "@react-kit/react-kit";
-import { useNavigate } from "react-router-dom";
-import { Divider } from "@mui/material";
+} from '@react-kit/react-kit';
+import { useNavigate } from 'react-router-dom';
 
 export default function ButtonsDemo() {
-  const navigate = useNavigate();
-  return (
-    <div style={{ marginInline: "1rem", textAlign: "center" }}>
-      <h2>Buttons Demo</h2>
-      <Divider sx={{ mb: 3 }} />
-      Cancel Button:
-      <CancelButton
-        onClick={() => console.log("Clicked Cancel Button")}
-        name={"Cancel"}
-      ></CancelButton>{" "}
-      <br />
-      Delete Button:
-      <DeleteButton loading={false} onClick={() => console.log("Clicked Delete Button")} /> <br />
-      <br />
-      <br />
-      Excel Button:
-      <ExcelButton onClick={() => console.log("Clicked Excel Button")}>
-        Export to Excel
-      </ExcelButton>{" "}
-      <br />
-      <br />
-      <div>
-        Success Button:
-        <SuccessButton onClick={() => console.log("Clicked Success Button")}>
-          Success
-        </SuccessButton>{" "}
-        <br />
-        <br />
-      </div>
-      <div>
-        Loading Success Button:
-        <LoadingSuccessButton
-          loading={false}
-          onClick={() => console.log("Clicked Loading Success Button")}
-        >
-          Save
-        </LoadingSuccessButton>
-        <br />
-      </div>
-      <div>
-        Go back Button:
-        <GoBackButton navigate={navigate}></GoBackButton>
-        <br />
-      </div>
-      <div>
-        History Button:
-        <HistoryButton onClick={() => console.log("Clicked History Button")}>History</HistoryButton>
-        <br />
-      </div>
-      <div>
-        Manage Button:
-        <ManageButton onClick={() => console.log("Clicked Manage Button")}>Manage</ManageButton>
-        <br />
-      </div>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div style={{ marginInline: '1rem', textAlign: 'center' }}>
+			<h2>Buttons Demo</h2>
+			<Divider sx={{ mb: 3 }} />
+			Cancel Button:
+			<CancelButton onClick={() => console.log('Clicked Cancel Button')} name={'Cancel'}></CancelButton> <br />
+			Delete Button:
+			<DeleteButton loading={false} onClick={() => console.log('Clicked Delete Button')} /> <br />
+			<br />
+			<br />
+			Excel Button:
+			<ExcelButton onClick={() => console.log('Clicked Excel Button')}>Export to Excel</ExcelButton> <br />
+			<br />
+			<div>
+				Success Button:
+				<SuccessButton onClick={() => console.log('Clicked Success Button')}>Success</SuccessButton> <br />
+				<br />
+			</div>
+			<div>
+				Loading Success Button:
+				<LoadingSuccessButton loading={false} onClick={() => console.log('Clicked Loading Success Button')}>
+					Save
+				</LoadingSuccessButton>
+				<br />
+			</div>
+			<div>
+				Go back Button:
+				<GoBackButton navigate={navigate}></GoBackButton>
+				<br />
+			</div>
+			<div>
+				History Button:
+				<HistoryButton onClick={() => console.log('Clicked History Button')}>History</HistoryButton>
+				<br />
+			</div>
+			<div>
+				Manage Button:
+				<ManageButton onClick={() => console.log('Clicked Manage Button')}>Manage</ManageButton>
+				<br />
+			</div>
+		</div>
+	);
 }
